9fca799d7a988a15fd5b265918b615fa0475c32a
Oron Port <oron.port@dfiant.works>
2025-09-02 23:42:18 +0300

    Max10 device is missing pulldown support indication

--- src/main/scala/dfhdl/platforms/devices/alteraintel/devices.scala
+++ src/main/scala/dfhdl/platforms/devices/alteraintel/devices.scala
@@ -1,5 +1,7 @@
 package dfhdl.platforms.devices.alteraintel
+import dfhdl.hw.constraints.io
 
+@io(missingPullDownSupport = true)
 class `10M50DAF484C6GES` extends Device, packages.F484:
   lazy val deviceName: String = "MAX10DA"
   lazy val partName: String = "10M50DAF484C6GES"
