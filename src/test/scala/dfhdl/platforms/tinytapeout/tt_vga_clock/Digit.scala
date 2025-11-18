ff73dd87e8cccf76b1c430842918014388f3f31e
Oron Port <oron.port@dfiant.works>
2025-11-18 23:56:12 +0200

    TT vga clock wip

--- src/test/scala/dfhdl/platforms/tinytapeout/tt_vga_clock/Digit.scala
+++ src/test/scala/dfhdl/platforms/tinytapeout/tt_vga_clock/Digit.scala
@@ -5,11 +5,12 @@ import dfhdl.*
     val DIGIT_INDEX_FILE: String = "tt_vga_clock/digit_index.hex",
     val COL_INDEX_FILE: String = "tt_vga_clock/col_index.hex",
     val COLOR_INDEX_FILE: String = "tt_vga_clock/color.bin",
-    val FONT_W: Int = 4,
-    val FONT_H: Int = 5,
-    val NUM_BLOCKS: Int = 32
+    val COLOR_BITS: Int = 6,
+    val FONT_W: Int <> CONST = 4,
+    val FONT_H: Int <> CONST = 5,
+    val NUM_BLOCKS: Int <> CONST = 32
 ) extends RTDesign:
-  // val COL_INDEX_W = $clog2(FONT_W);
+  val COL_INDEX_W = clog2(FONT_W)
   val x_block = UInt(6) <> IN
 
   /** the number to display: [0->9] */
@@ -18,19 +19,15 @@ import dfhdl.*
   /** shift through the colours */
   val color_offset = UInt(4) <> IN
   val digit_index = UInt(6) <> OUT.REG
-  val color = UInt(6) <> OUT.REG
-  val col_index = UInt.until(FONT_W) <> OUT.REG
+  val color = UInt(COLOR_BITS) <> OUT.REG
+  val col_index = UInt(COL_INDEX_W) <> OUT.REG
 
-  val digit_index_mem =
-    UInt(6) X 12 <> VAR initFile (DIGIT_INDEX_FILE, InitFileFormat.VerilogHex)
-  val col_index_mem =
-    UInt.until(FONT_W) X NUM_BLOCKS <> VAR initFile (COL_INDEX_FILE, InitFileFormat.VerilogHex)
-  val color_index_mem =
-    UInt(6) X 8 <> VAR initFile (COLOR_INDEX_FILE, InitFileFormat.VerilogBin)
+  val digit_index_mem = UInt(6) X 12 <> VAR initFile DIGIT_INDEX_FILE
+  val col_index_mem = UInt(COL_INDEX_W) X NUM_BLOCKS <> VAR initFile COL_INDEX_FILE
+  val color_index_mem = UInt(COLOR_BITS) X 8 <> VAR initFile COLOR_INDEX_FILE
 
-  val char = x_block.bits(5, 2)
+  val char = x_block.bits(5, 2).uint
   digit_index.din := digit_index_mem(number)
-  val col_index_mem_addr: UInt[5] <> VAL = (x_block min (NUM_BLOCKS - 1)).truncate
-  col_index.din := col_index_mem(col_index_mem_addr)
-  color.din := color_index_mem((char + color_offset).resize(3))
+  col_index.din := col_index_mem((x_block min (NUM_BLOCKS - 1)).truncate)
+  color.din := color_index_mem((char + color_offset).truncate)
 end Digit
